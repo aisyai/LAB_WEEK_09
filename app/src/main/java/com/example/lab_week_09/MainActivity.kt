c909bfe89ad55046e7683a561b757d233d9773e3
aisyai <aisyaadiyann@gmail.com>
2025-11-04 21:05:03 +0700

    Commit No. 3 – UI Element and Theme

--- app/src/main/java/com/example/lab_week_09/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_09/MainActivity.kt
@@ -9,10 +9,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf // Added import for mutableStateListOf
@@ -26,6 +24,9 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lab_week_09.ui.theme.LAB_WEEK_09Theme
+import com.example.lab_week_09.ui.theme.OnBackgroundTitleText
+import com.example.lab_week_09.ui.theme.PrimaryTextButton
+import com.example.lab_week_09.ui.theme.OnBackgroundItemText
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -101,56 +102,54 @@ fun HomeContent(
 ) {
     // Here, we use LazyColumn to display a list of items Lazily
     LazyColumn {
-        // Here, we use item to display an item inside the LazyColumn
+        //Here, we use item to display an item inside the LazyColumn
         item {
             Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxSize(),
-                // Alignment.CenterHorizontally is used to align the Column horizontally
-                horizontalAlignment = Alignment.CenterHorizontally
+                //Modifier.padding(16.dp) is used to add padding to the Column
+                //You can also use Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+            //to add padding horizontally and vertically
+            //or Modifier.padding(start = 16.dp, top = 8.dp, end = 16.dp, bottom = 8.dp)
+            //to add padding to each side
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize(),
+            //Alignment.CenterHorizontally is used to align the Column horizontally
+            //You can also use verticalArrangement = Arrangement.Center to align the Column vertically
+                    horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(
-                    text = stringResource(
-                        id = R.string.enter_item
-                    )
-                )
-                // Here, we use TextField to display a text input field
-                TextField(
-                    // Set the value of the input field
-                    value = inputField.name,
-                    // Set the keyboard type of the input field
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Text // Changed to KeyboardType.Text (Commit 2, Step 4)
-                    ),
-                    // Set what happens when the value of the input field changes
-                    onValueChange = {
-                        // Here, we call the onInputValueChange Lambda function
-                        // and pass the value of the input field as a parameter
-                        // This is so that we can update the value of the inputField
-                        onInputValueChange(it)
-                    }
-                )
-                // Here, we use Button to display a button
-                // the onClick parameter is used to set what happens when the button is clicked
-                Button(onClick = {
-                    // Here, we call the onButtonClick Lambda function
-                    // This is so that we can add the inputField value to the ListData
-                    // and reset the value of the inputField
+            //Here, we call the OnBackgroundTitleText UI Element
+            OnBackgroundTitleText(text = stringResource(
+                id = R.string.enter_item)
+            )
+
+            //Here, we use TextField to display a text input field
+            TextField(
+                //Set the value of the input field
+                value = inputField.name,
+                //Set the keyboard type of the input field
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text
+                ),
+                //Set what happens when the value of the input field changes
+                onValueChange = {
+                    //Here, we call the onInputValueChange lambda function
+                    //and pass the value of the input field as a parameter
+                    //This is so that we can update the value of the inputField
+                    onInputValueChange(it)
+                }
+            )
+
+                //Here, we call the PrimaryTextButton UI Element
+                PrimaryTextButton(text = stringResource(
+                    id = R.string.button_click)
+                ) {
                     onButtonClick()
-                }) {
-                    // Set the text of the button
-                    Text(
-                        text = stringResource(
-                            id = R.string.button_click
-                        )
-                    )
                 }
             }
         }
-        // Here, we use items to display a List of items inside the LazyColumn
-        // This is the RecyclerView replacement
-        // We pass the listData as a parameter
+        //Here, we use items to display a list of items inside the LazyColumn
+        //This is the RecyclerView replacement
+        //We pass the listData as a parameter
         items(listData) { item ->
             Column(
                 modifier = Modifier
@@ -158,12 +157,13 @@ fun HomeContent(
                     .fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                // Now displaying item.name (Commit 2, Step 4)
-                Text(text = item.name)
+                //Here, we call the OnBackgroundItemText UI Element
+                OnBackgroundItemText(text = item.name)
             }
         }
     }
-}
+
+                }
 
 // Here, we create a preview function of the Home composable (Commit 2 is before the new PreviewHome)
 @Preview(showBackground = true)
